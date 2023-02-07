5a41092848e016c83db0f00b372a7b0696e4c67b
zikazaki <17160061@su.edu.ye>
2023-02-07 23:37:11 +0300

    Update session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,7 @@
-Rails.application.config.session_store :cookie_store, key: "_vespa_backend_session", domain: "vespa-backend-heroku.herokuapp.com"
+if Rails.env == "production"
+  # Make sure to change the domain to match your production domain name before deploying. 
+  Rails.application.config.session_store :cookie_store, key: "_vespa_backend_session", domain: "vespa-backend-heroku.herokuapp.com"
+else
+  # This is the default domain for development and test environments. 
+  Rails.application.config.session_store :cookie_store, key: "_vespa_backend_session"
+end
