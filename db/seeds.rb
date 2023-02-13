7128e53e401e613213678479b7a6db086a670523
zikazaki <17160061@su.edu.ye>
2023-02-13 23:23:55 +0300

    Add [admin] column to users model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Create a regular user.
+User.create!(
+    username: "test",
+    email: "test@gmail.com",
+    password: "test123",
+    password_confirmation: "test123",
+    admin: false
+)
+
+# Create admin user.
+User.create!(
+    username: "admin",
+    email: "admin@gmail.com",
+    password: "admin123",
+    password_confirmation: "admin123",
+    admin: true
+)
