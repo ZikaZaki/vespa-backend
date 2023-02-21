5113beb06f0b08b5bf9d672afce311915eb850b7
zikazaki <17160061@su.edu.ye>
2023-02-21 07:01:42 +0300

    Fix linter errors

--- test/controllers/reservations_controller_test.rb
+++ test/controllers/reservations_controller_test.rb
@@ -1,45 +1,63 @@
-require "test_helper"
+require 'test_helper'
 
 class ReservationsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @reservation = reservations(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get reservations_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_reservation_url
     assert_response :success
   end
 
-  test "should create reservation" do
-    assert_difference("Reservation.count") do
-      post reservations_url, params: { reservation: { city_id: @reservation.city_id, motorcycle_id: @reservation.motorcycle_id, reserve_date: @reservation.reserve_date, returning_date: @reservation.returning_date, user_id: @reservation.user_id } }
+  test 'should create reservation' do
+    assert_difference('Reservation.count') do
+      post reservations_url,
+           params: {
+             reservation: {
+               city_id: @reservation.city_id,
+               motorcycle_id: @reservation.motorcycle_id,
+               reserve_date: @reservation.reserve_date,
+               returning_date: @reservation.returning_date,
+               user_id: @reservation.user_id
+             }
+           }
     end
 
     assert_redirected_to reservation_url(Reservation.last)
   end
 
-  test "should show reservation" do
+  test 'should show reservation' do
     get reservation_url(@reservation)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_reservation_url(@reservation)
     assert_response :success
   end
 
-  test "should update reservation" do
-    patch reservation_url(@reservation), params: { reservation: { city_id: @reservation.city_id, motorcycle_id: @reservation.motorcycle_id, reserve_date: @reservation.reserve_date, returning_date: @reservation.returning_date, user_id: @reservation.user_id } }
+  test 'should update reservation' do
+    patch reservation_url(@reservation),
+          params: {
+            reservation: {
+              city_id: @reservation.city_id,
+              motorcycle_id: @reservation.motorcycle_id,
+              reserve_date: @reservation.reserve_date,
+              returning_date: @reservation.returning_date,
+              user_id: @reservation.user_id
+            }
+          }
     assert_redirected_to reservation_url(@reservation)
   end
 
-  test "should destroy reservation" do
-    assert_difference("Reservation.count", -1) do
+  test 'should destroy reservation' do
+    assert_difference('Reservation.count', -1) do
       delete reservation_url(@reservation)
     end
 
--- test/system/reservations_test.rb
+++ test/system/reservations_test.rb
@@ -1,49 +1,49 @@
-require "application_system_test_case"
+require 'application_system_test_case'
 
 class ReservationsTest < ApplicationSystemTestCase
   setup do
     @reservation = reservations(:one)
   end
 
-  test "visiting the index" do
+  test 'visiting the index' do
     visit reservations_url
-    assert_selector "h1", text: "Reservations"
+    assert_selector 'h1', text: 'Reservations'
   end
 
-  test "should create reservation" do
+  test 'should create reservation' do
     visit reservations_url
-    click_on "New reservation"
+    click_on 'New reservation'
 
-    fill_in "City", with: @reservation.city_id
-    fill_in "Motorcycle", with: @reservation.motorcycle_id
-    fill_in "Reserve date", with: @reservation.reserve_date
-    fill_in "Returning date", with: @reservation.returning_date
-    fill_in "User", with: @reservation.user_id
-    click_on "Create Reservation"
+    fill_in 'City', with: @reservation.city_id
+    fill_in 'Motorcycle', with: @reservation.motorcycle_id
+    fill_in 'Reserve date', with: @reservation.reserve_date
+    fill_in 'Returning date', with: @reservation.returning_date
+    fill_in 'User', with: @reservation.user_id
+    click_on 'Create Reservation'
 
-    assert_text "Reservation was successfully created"
-    click_on "Back"
+    assert_text 'Reservation was successfully created'
+    click_on 'Back'
   end
 
-  test "should update Reservation" do
+  test 'should update Reservation' do
     visit reservation_url(@reservation)
-    click_on "Edit this reservation", match: :first
+    click_on 'Edit this reservation', match: :first
 
-    fill_in "City", with: @reservation.city_id
-    fill_in "Motorcycle", with: @reservation.motorcycle_id
-    fill_in "Reserve date", with: @reservation.reserve_date
-    fill_in "Returning date", with: @reservation.returning_date
-    fill_in "User", with: @reservation.user_id
-    click_on "Update Reservation"
+    fill_in 'City', with: @reservation.city_id
+    fill_in 'Motorcycle', with: @reservation.motorcycle_id
+    fill_in 'Reserve date', with: @reservation.reserve_date
+    fill_in 'Returning date', with: @reservation.returning_date
+    fill_in 'User', with: @reservation.user_id
+    click_on 'Update Reservation'
 
-    assert_text "Reservation was successfully updated"
-    click_on "Back"
+    assert_text 'Reservation was successfully updated'
+    click_on 'Back'
   end
 
-  test "should destroy Reservation" do
+  test 'should destroy Reservation' do
     visit reservation_url(@reservation)
-    click_on "Destroy this reservation", match: :first
+    click_on 'Destroy this reservation', match: :first
 
-    assert_text "Reservation was successfully destroyed"
+    assert_text 'Reservation was successfully destroyed'
   end
 end
